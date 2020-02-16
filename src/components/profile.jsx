974727ce8dfd5d41555d2987a40382325cbbc002
denys1984 <denysgdmgroup@gmail.com>
2020-02-16 12:58:41 +0200

    lesson 12

---
+++ src/components/profile.jsx
@@ -0,0 +1,27 @@
+import React from 'react';
+
+const Profile = () => {
+    return (
+        <div className="content">
+        <div>
+          <img src="https://p.bigstockphoto.com/GeFvQkBbSLaMdpKXF1Zv_bigstock-Aerial-View-Of-Blue-Lakes-And--227291596.jpg" alt=""/>
+        </div>
+        <div>
+          {/* <img src="https://miro.medium.com/max/1200/1*mk1-6aYaf_Bes1E3Imhc0A.jpeg" alt=""/> */}
+        </div>
+        <div>
+          my post
+          <div>
+            new post
+          </div>
+          <div>
+            <div>post1</div>
+            <div>post2</div>
+          </div>
+
+        </div>
+      </div>
+    )
+}
+
+export default Profile;
