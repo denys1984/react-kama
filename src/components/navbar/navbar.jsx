4951a5c0987e657a70cd67a9dae77fe9ff90f6a8
denys1984 <denysgdmgroup@gmail.com>
2020-02-20 16:07:18 +0200

    lesson 20

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,14 +1,25 @@
 import React from 'react';
 import navstyle from './navbar.module.css'
+import { NavLink } from 'react-router-dom';
 
 const Navbar = () => {
     return (
     <nav className={navstyle.nav}>
-        <div className={navstyle.item}><a href="/profile">Profile</a></div>
-        <div className={`${navstyle.item} ${navstyle.active}`}><a href="/dialogs">Messages</a></div>
-        <div className={navstyle.item}><a href="">News</a></div>
-        <div className={navstyle.item}><a href="">Music</a></div>
-        <div className={navstyle.item}><a href="">Settings</a></div>
+        <div className={navstyle.item}>
+          <NavLink to="/profile" activeClassName={navstyle.activeLink}>Profile</NavLink>
+        </div>
+        <div className={`${navstyle.item} ${navstyle.active}`}>
+          <NavLink to="/dialogs" activeClassName={navstyle.activeLink}>Messages</NavLink>
+        </div>
+        <div className={navstyle.item}>
+          <NavLink to="">News</NavLink>
+        </div>
+        <div className={navstyle.item}>
+          <NavLink to="">Music</NavLink>
+        </div>
+        <div className={navstyle.item}>
+          <NavLink to="">Settings</NavLink>
+        </div>
       </nav>
     )
 }
